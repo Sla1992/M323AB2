9f477f4f65f0b0943945dd9d71168060beae5d62
Sla <sla_psn@hotmail.com>
2026-02-14 10:52:07 +0100

    add array with two objects

---
+++ immutable_arrays.js
@@ -0,0 +1,14 @@
+// array with two objects
+
+const users = [
+  {
+    id: 1,
+    name: "Lukas",
+    job: "Information Technologist"
+  },
+  {
+    id: 2,
+    name: "Anna",
+    job: "Software Developer"
+  }
+];
